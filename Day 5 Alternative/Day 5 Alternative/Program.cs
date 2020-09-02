2ce6b578d55036a324662a06f5fcdd6191eb98e9
BSAlberto <BSAlberto@gmail.com>
2020-09-02 09:39:26 -0400

    First push from Alternative

---
+++ Day 5 Alternative/Day 5 Alternative/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_5_Alternative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from alternative!");
+        }
+    }
+}
